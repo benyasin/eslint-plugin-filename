b3b675007aa5e257860e388136385037287ccce0
wangbingying <wangbingying@kuaishou.com>
2021-03-29 17:40:31 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # eslint-plugin-filename
+
+Adds an ESLint rule to enforce filename conventions for linted files. Allows different options for different file extensions. Supports custom regular expressions.
+
+[![NPM Version][npm-image]][npm-url]
+[![NPM Downloads][downloads-image]][downloads-url]
+[![Build Status][travis-image]][travis-url]
+[![Coverage Status][coverage-image]][coverage-url]
+[![License: MIT][license-image]][license-url]
+
+
+## Installation
+
+```bash
+$ npm install -D eslint-plugin-filename
+```
+
+The following built-in values are supported: `pascalcase`/`PascalCase`, `camelcase`/`camelCase`, `snakecase`/`snake_case`, `kebabcase`/`kebab-case`.
+Multiple built-in values passed as array are also supported.
+
+Add it to your `.eslintrc.js`:
+
+```js
+module.exports = {
+  plugins: [
+    'filename',
+  ],
+  rules: {
+    'filename/match': [2, 'camelcase'],
+  },
+};
+```
+
+or
+
+```js
+module.exports = {
+  plugins: [
+    'filename',
+  ],
+  rules: {
+    'filename/match': [2, ['camelcase','pascalcase']],
+  },
+};
+```
+
+## Plugin Options
+
+You can also provide your own regex, if you are using your own regex, do not pass them as array.
+
+```js
+'filename-rules/match': [2, /^([a-z]+-)*[a-z]+(?:\..*)?$/],
+```
+
+You can also specify different options for different file extensions. In this case the plugin will only check files with extensions you explicitly provided:
+
+```js
+'filename-rules/match': [2, { '.js': 'camelCase', '.ts': /^([a-z]+-)*[a-z]+(?:\..*)?$/ }],
+```
+
+## License
+
+MIT
+
+[npm-image]: https://img.shields.io/npm/v/eslint-plugin-filename.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/eslint-plugin-filename
+[downloads-image]: https://img.shields.io/npm/dm/eslint-plugin-filename.svg?style=flat-square
+[downloads-url]: https://npmjs.org/package/eslint-plugin-filename
+[travis-image]: https://img.shields.io/travis/dolsem/eslint-plugin-filename.svg?style=flat-square
+[travis-url]: https://travis-ci.org/dolsem/eslint-plugin-filename
+[coverage-image]: https://img.shields.io/coveralls/dolsem/eslint-plugin-filename.svg?style=flat-square
+[coverage-url]: https://coveralls.io/github/dolsem/eslint-plugin-filename?branch=master
+[license-image]: https://img.shields.io/badge/License-MIT-blue.svg?style=flat-square
+[license-url]: https://opensource.org/licenses/MIT
---
+++ test/lib/match.test.js
@@ -0,0 +1,389 @@
+import test from 'ava';
+import { RuleTester } from 'eslint';
+
+import * as match from '../../lib/match';
+
+const tester = new RuleTester();
+const testRule = (tests) => () => tester.run('match', match, tests);
+const code = '"hello world";';
+
+test('single regex', testRule({
+  valid: [
+    {
+      code,
+      filename: '/foo/bar/test.txt',
+      options: [/^test(?:\..*)?$/],
+    },
+  ],
+  invalid: [
+    {
+      code,
+      filename: '/foo/bar/test_.txt',
+      options: [/^test(?:\..*)?$/],
+      errors: [
+        { message: "Filename 'test_.txt' does not match /^test(?:\\..*)?$/.", column: 1, line: 1 },
+      ],
+    },
+  ],
+}));
+
+test('throws on unknown alias', (t) => t.throws(testRule({
+  valid: [
+    {
+      code,
+      filename: '/foo/bar/module.js',
+      options: ['bla'],
+    },
+  ],
+  invalid: [],
+}), 'Unrecognized option "bla"\nOccurred while linting /foo/bar/module.js:1'));
+
+['camelcase', 'camelCase'].forEach((alias) => {
+  test(`single alias - ${alias}`, testRule({
+    valid: [
+      {
+        code,
+        filename: '/foo/bar/module.js',
+        options: [alias],
+      },
+      {
+        code,
+        filename: '/foo/bar/anotherModule.js',
+        options: [alias],
+      },
+      {
+        code,
+        filename: '/foo/bar/module.test.js',
+        options: [alias],
+      },
+    ],
+    invalid: [
+      {
+        code,
+        filename: '/foo/bar/Module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'Module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/AnotherModule.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'AnotherModule.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/another-module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'another-module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/another_module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'another_module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+    ],
+  }));
+});
+
+['pascalcase', 'PascalCase'].forEach((alias) => {
+  test(`single alias - ${alias}`, testRule({
+    valid: [
+      {
+        code,
+        filename: '/foo/bar/Url.js',
+        options: [alias],
+      },
+      {
+        code,
+        filename: '/foo/bar/AnotherModule.js',
+        options: [alias],
+      },
+      {
+        code,
+        filename: '/foo/bar/AnotherModule.test.js',
+        options: [alias],
+      },
+      {
+        code,
+        filename: '/foo/bar/ModuleNameWithACRONYM.js',
+        options: [alias],
+      },
+    ],
+    invalid: [
+      {
+        code,
+        filename: '/foo/bar/URL.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'URL.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/url.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'url.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/anotherModule.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'anotherModule.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/Another-Module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'Another-Module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/Another_Module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'Another_Module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+    ],
+  }));
+});
+
+['snakecase', 'snake_case'].forEach((alias) => {
+  test(`single alias - ${alias}`, testRule({
+    valid: [
+      {
+        code,
+        filename: '/foo/bar/module.js',
+        options: [alias],
+      },
+      {
+        code,
+        filename: '/foo/bar/another_module.js',
+        options: [alias],
+      },
+      {
+        code,
+        filename: '/foo/bar/another_module.test.js',
+        options: [alias],
+      },
+    ],
+    invalid: [
+      {
+        code,
+        filename: '/foo/bar/Module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'Module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/AnotherModule.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'AnotherModule.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/anotherModule.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'anotherModule.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/another-module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'another-module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/Another_Module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'Another_Module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+    ],
+  }));
+});
+
+['kebabcase', 'kebab-case'].forEach((alias) => {
+  test(`single alias - ${alias}`, testRule({
+    valid: [
+      {
+        code,
+        filename: '/foo/bar/module.js',
+        options: [alias],
+      },
+      {
+        code,
+        filename: '/foo/bar/another-module.js',
+        options: [alias],
+      },
+      {
+        code,
+        filename: '/foo/bar/another-module.test.js',
+        options: [alias],
+      },
+    ],
+    invalid: [
+      {
+        code,
+        filename: '/foo/bar/Module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'Module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/AnotherModule.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'AnotherModule.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/anotherModule.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'anotherModule.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/Another-Module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'Another-Module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+      {
+        code,
+        filename: '/foo/bar/another_module.js',
+        options: [alias],
+        errors: [
+          { message: `Filename 'another_module.js' does not match ${alias}.`, column: 1, line: 1 },
+        ],
+      },
+    ],
+  }));
+});
+
+test('file extension mapping', testRule({
+  valid: [
+    {
+      code,
+      filename: '/foo/bar/someModule.js',
+      options: [{ '.js': 'camelcase', '.ts': /^([a-z]+-)*[a-z]+(?:\..*)?$/ }],
+    },
+    {
+      code,
+      filename: '/foo/bar/some-module.ts',
+      options: [{ '.js': 'camelcase', '.ts': /^([a-z]+-)*[a-z]+(?:\..*)?$/ }],
+    },
+    {
+      code,
+      filename: '/foo/bar/SkippedModule.jsx',
+      options: [{ '.js': 'camelcase', '.ts': /^([a-z]+-)*[a-z]+(?:\..*)?$/ }],
+    },
+  ],
+  invalid: [
+    {
+      code,
+      filename: '/foo/bar/some-module.js',
+      options: [{ '.js': 'camelcase', '.ts': /^([a-z]+-)*[a-z]+(?:\..*)?$/ }],
+      errors: [
+        { message: "Filename 'some-module.js' does not match camelcase.", column: 1, line: 1 },
+      ],
+    },
+    {
+      code,
+      filename: '/foo/bar/someModule.ts',
+      options: [{ '.js': 'camelcase', '.ts': /^([a-z]+-)*[a-z]+(?:\..*)?$/ }],
+      errors: [
+        { message: "Filename 'someModule.ts' does not match /^([a-z]+-)*[a-z]+(?:\\..*)?$/.", column: 1, line: 1 },
+      ],
+    },
+  ],
+}));
+
+test(`multiple alias`, testRule({
+  valid: [
+    {
+      code,
+      filename: '/foo/bar/Url.js',
+      options: [['camelcase','PascalCase']],
+    },
+    {
+      code,
+      filename: '/foo/bar/AnotherModule.js',
+      options: [['camelcase','PascalCase']],
+    },
+    {
+      code,
+      filename: '/foo/bar/AnotherModule.test.js',
+      options: [['camelcase','PascalCase']],
+    },
+    {
+      code,
+      filename: '/foo/bar/ModuleNameWithACRONYM.js',
+      options: [['camelcase','PascalCase']],
+    },
+    {
+      code,
+      filename: '/foo/bar/module.js',
+      options: [['camelcase','PascalCase']],
+    },
+    {
+      code,
+      filename: '/foo/bar/anotherModule.js',
+      options: [['camelcase','PascalCase']],
+    },
+    {
+      code,
+      filename: '/foo/bar/module.test.js',
+      options: [['camelcase','PascalCase']],
+    },
+  ],
+  invalid: [
+    {
+      code,
+      filename: '/foo/bar/another-module.js',
+      options: [['camelcase','PascalCase']],
+      errors: [
+        { message: `Filename 'another-module.js' does not match camelcase,PascalCase.`, column: 1, line: 1 },
+      ],
+    },
+    {
+      code,
+      filename: '/foo/bar/another_module.js',
+      options: [['camelcase','PascalCase']],
+      errors: [
+        { message: `Filename 'another_module.js' does not match camelcase,PascalCase.`, column: 1, line: 1 },
+      ],
+    },
+  ],
+}));
+
